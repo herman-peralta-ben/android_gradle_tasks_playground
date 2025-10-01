3f6f4f7444448410c383fcbb4f48d7760948ae0e
Herman Peralta <herman.peralta.ben@gmail.com>
2025-10-01 15:01:51 -0600

    Create Project

---
+++ app/src/main/java/com/herman/android_gradle_tasks_playground/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.herman.android_gradle_tasks_playground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.herman.android_gradle_tasks_playground.ui.theme.Android_gradle_tasks_playgroundTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android_gradle_tasks_playgroundTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Android_gradle_tasks_playgroundTheme {
+        Greeting("Android")
+    }
+}
